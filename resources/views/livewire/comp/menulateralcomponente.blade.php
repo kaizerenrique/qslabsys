dce105875bf47dca2eaa7500c1907532c8ddf3cd
Oliver Gomez <kayserenrique@gmail.com>
2023-05-09 11:35:18 -0400

    reinstall

--- resources/views/livewire/comp/menulateralcomponente.blade.php
+++ resources/views/livewire/comp/menulateralcomponente.blade.php
@@ -80,10 +80,14 @@
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+                            d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" >
+                            </path>
                         </svg>
                     </span>
-                    <span class="ml-2 text-sm tracking-wide truncate">Profile</span>
+                    <span class="ml-2 text-sm tracking-wide truncate">Usuarios</span>
+                    <span class="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-500 bg-red-50 rounded-full">
+                        1.2k
+                    </span>
                 </a>
             </li>
             <li>
@@ -108,3 +112,5 @@
     </div>
 </div>
 <!-- ./Sidebar -->
+
+  
