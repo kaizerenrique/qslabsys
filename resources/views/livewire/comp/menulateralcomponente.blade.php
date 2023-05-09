6a88e184a94c15b3d6ec92f9fd37e9fdd4be17fb
Oliver Gomez <kayserenrique@gmail.com>
2023-05-09 12:00:54 -0400

    agregar secciones de permiso y rol al menu

--- resources/views/livewire/comp/menulateralcomponente.blade.php
+++ resources/views/livewire/comp/menulateralcomponente.blade.php
@@ -90,6 +90,40 @@
                     </span>
                 </a>
             </li>
+            <li class="px-5 hidden md:block">
+                <div class="flex flex-row items-center mt-5 h-8">
+                    <div class="text-sm font-light tracking-wide text-gray-400 uppercase">Configuraciones</div>
+                </div>
+            </li>
+            <li>
+                <a href="#"
+                    class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
+                    <span class="inline-flex justify-center items-center ml-4">
+                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6" />
+                        </svg>
+                    </span>
+                    <span class="ml-2 text-sm tracking-wide truncate">Permisos</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                    class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
+                    <span class="inline-flex justify-center items-center ml-4">
+                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3">
+                            </path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                        </svg>
+                    </span>
+                    <span class="ml-2 text-sm tracking-wide truncate">Roles</span>
+                </a>
+            </li>
             <li>
                 <a href="#"
                     class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
@@ -103,7 +137,7 @@
                                 d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
                         </svg>
                     </span>
-                    <span class="ml-2 text-sm tracking-wide truncate">Settings</span>
+                    <span class="ml-2 text-sm tracking-wide truncate">Configuraciones</span>
                 </a>
             </li>
         </ul>
@@ -112,5 +146,10 @@
     </div>
 </div>
 <!-- ./Sidebar -->
+
+
+
+
+
 
   
